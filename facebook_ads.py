842b2fd7d37b6ad800fe9be64a34351a51b299c2
DrDeathBubbles <aaron.meagher@gmail.com>
2017-10-10 12:50:26 +0100

    Adding the init class method

--- facebook_ads.py
+++ facebook_ads.py
@@ -1,12 +1,18 @@
+
 import os
-import fa
+import logging
+from facebookads import FacebookAdsApi
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 class facebook_ads:
     """
     A class for the creating of ad campaigns, adding adsets and ad creative
     """
 
-    def __init__:
+    def __init__(self):
         """
         Initialising the FacebookAdsApi
         """
@@ -14,3 +20,8 @@ class facebook_ads:
             app_id = os.environ['APPID']
             app_secret = os.environ['APPSECRET']
             access_token = os.environ['ACCESSTOKEN']
+
+            FacebookAdsApi.init(app_id, app_secret, access_token)
+
+        except KeyError as ke:
+            logger.error('You need to set the following environment variable \n {}'.format(ke))
