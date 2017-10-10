e10e20a8e650f765698d93140608df57a7976b68
DrDeathBubbles <aaron.meagher@gmail.com>
2017-10-10 15:09:32 +0100

    Finishe. Will publish an add to Facebook

--- facebook_ads.py
+++ facebook_ads.py
@@ -10,7 +10,11 @@ from facebookads.adobjects.targeting import Targeting
 from facebookads.adobjects.adaccount import AdAccount
 from facebookads.adobjects.adset import AdSet
 from facebookads.adobjects.adimage import AdImage
-
+from facebookads.adobjects.adcreative import AdCreative
+from facebookads.adobjects.adcreativelinkdata import AdCreativeLinkData
+from facebookads.adobjects.adcreativeobjectstoryspec \
+    import AdCreativeObjectStorySpec
+from facebookads.adobjects.ad import Ad
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -99,6 +103,36 @@ class facebook_ads:
         image.remote_create()
         
         return image
+    
+    def create_ad_creative(self,message,link,image_hash,page_id,act_id,adcreative_name):
+        link_data = AdCreativeLinkData()
+        link_data[AdCreativeLinkData.Field.message] = message 
+        link_data[AdCreativeLinkData.Field.link] = link 
+        link_data[AdCreativeLinkData.Field.image_hash] = image_hash 
+
+        object_story_spec = AdCreativeObjectStorySpec()
+        object_story_spec[AdCreativeObjectStorySpec.Field.page_id] = page_id 
+        object_story_spec[AdCreativeObjectStorySpec.Field.link_data] = link_data
+
+        creative = AdCreative(parent_id='act_{}'.format(act_id))
+        creative[AdCreative.Field.name] = 'AdCreative for Link Ad'
+        creative[AdCreative.Field.object_story_spec] = object_story_spec
+        creative.remote_create()
+
+        return(creative)
+
+    def schedule_ads(self,act_id, ad_name,ad_set_id, creative_id):
+        ad = Ad(parent_id='act_{}'.format(act_id))
+        ad[Ad.Field.name] = ad_name
+        ad[Ad.Field.adset_id] = ad_set_id 
+        ad[Ad.Field.creative] = {
+            'creative_id': creative_id,
+        }
+        ad.remote_create(params={
+            'status': Ad.Status.paused,
+        })
+
+        return ad
 
 
 if __name__ == '__main__':
@@ -107,3 +141,8 @@ if __name__ == '__main__':
     targeting = a.targeting_search('Xbox')
     targeting_params = a.targeting_parameters(targeting)
     adset = a.ad_set_creation(851394958259883,'AJM_ADSET_TEST',campaign['id'],targeting_params)
+    image = a.create_ad_image(851394958259883,'/Users/aaronmeagher/Desktop/San_Juan.jpg')
+    ad_creative = a.create_ad_creative('This is the AdCreativeLinkData.Field.message',
+    'websummit.com',image['hash'],294067420659309,851394958259883,'AJM_TEST_CREATIVE'
+    )
+    ad = a.schedule_ads(851394958259883,'AJM_TEST',adset['id'],ad_creative['id'])
