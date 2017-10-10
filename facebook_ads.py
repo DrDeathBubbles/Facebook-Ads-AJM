50048748bbde9b8682a34d4259eb5c678154d9d1
DrDeathBubbles <aaron.meagher@gmail.com>
2017-10-10 14:20:24 +0100

    Adding create image method

--- facebook_ads.py
+++ facebook_ads.py
@@ -1,11 +1,16 @@
 
 import os
 import logging
+import datetime
 
 from facebookads import FacebookAdsApi
 from facebookads.adobjects.campaign import Campaign
 from facebookads.adobjects.targetingsearch import TargetingSearch
 from facebookads.adobjects.targeting import Targeting
+from facebookads.adobjects.adaccount import AdAccount
+from facebookads.adobjects.adset import AdSet
+from facebookads.adobjects.adimage import AdImage
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -54,6 +59,7 @@ class facebook_ads:
         return resp
 
     def targeting_parameters(self,interests):
+        interests = [interest['id'] for interest in interests]
         targeting = {
             Targeting.Field.geo_locations: {
                 Targeting.Field.countries: ['US']
@@ -61,3 +67,43 @@ class facebook_ads:
             Targeting.Field.interests: interests,
 
         }
+        return targeting
+
+
+    def ad_set_creation(self,act_id,ad_set_name,campaign_id,targeting):
+        today = datetime.date.today()
+        start_time = str(today + datetime.timedelta(weeks=1))
+        end_time = str(today + datetime.timedelta(weeks=2))
+
+        ad_account = AdAccount(fbid='act_{}'.format(act_id))
+
+        params = {
+            AdSet.Field.name: ad_set_name ,
+            AdSet.Field.campaign_id: campaign_id,
+            AdSet.Field.daily_budget: 1000,
+            AdSet.Field.billing_event: AdSet.BillingEvent.impressions,
+            AdSet.Field.optimization_goal: AdSet.OptimizationGoal.reach,
+            AdSet.Field.bid_amount: 2,
+            AdSet.Field.targeting: targeting,
+            AdSet.Field.start_time: start_time,
+            AdSet.Field.end_time: end_time,
+            AdSet.Field.status: AdSet.Status.active,
+        }
+        adset = ad_account.create_ad_set(params=params)
+
+        return adset
+
+    def create_ad_image(self,act_id,image_path):
+        image = AdImage(parent_id = 'act_{}'.format(act_id))
+        image[AdImage.Field.filename] = image_path 
+        image.remote_create()
+        
+        return image
+
+
+if __name__ == '__main__':
+    a = facebook_ads()
+    campaign = a.create_campaign(851394958259883,'AJM_TEST_NES') 
+    targeting = a.targeting_search('Xbox')
+    targeting_params = a.targeting_parameters(targeting)
+    adset = a.ad_set_creation(851394958259883,'AJM_ADSET_TEST',campaign['id'],targeting_params)
