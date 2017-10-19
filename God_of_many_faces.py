7fb0a8943fc30f19cc75c2f6220b790e386b731d
DrDeathBubbles <aaron.meagher@gmail.com>
2017-10-19 12:16:13 +0100

    Changing names and adding control script

---
+++ God_of_many_faces.py
@@ -0,0 +1,11 @@
+#This is a file for the creation of audicence from phonebooks
+#And creating ads which use the photos of these phonebooks
+#To make targeted ads
+
+from Audience_tools import *
+from Ads_tools import *
+
+
+
+
+if __name__ == "__main__":
